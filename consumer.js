cfffc0f3e2cbd25294b30416eab226a723616c04
pelvinaydin <pelvin16@gmail.com>
2020-08-07 15:44:17 +0300

    project LogOp

---
+++ consumer.js
@@ -0,0 +1,55 @@
+const express = require('express');
+var app = express();
+var mongoose = require("mongoose");
+var amqp = require('amqplib/callback_api');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect('mongodb://localhost/logop', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.set('useFindAndModify', false);
+
+
+var logSchema = new mongoose.Schema({
+    CreatedTime : Date,
+     logObject  : Object
+ });
+
+var logging = mongoose.model('logging', logSchema, 'objlog');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        } 
+        var queue = 'logChannel';
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        //console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function (data) {
+            objList = JSON.parse(data.content.toString())
+            
+            for(var i = 0; i < objList.length; i++)
+            {
+                var objLog = new logging(objList[i]);
+                var result = objLog.save();
+            }
+
+            //Write MongoDB-------------------------
+            
+            //--------------------------------------
+            
+            console.log("MongoDB'ye Kaydedilen Veri : %s", obj);
+        }, {
+            noAck: true
+        });
+    });
+});
---
+++ service.js
@@ -0,0 +1,70 @@
+const express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+var amqp = require('amqplib/callback_api');
+const rabbitUrl = 'amqp://localhost';
+
+function sendRabbitMQ(logChannel, data){
+
+    amqp.connect('amqp://localhost', function(error0, connection) {
+      
+        if (error0) {
+            throw error0;  
+        }
+        connection.createChannel(function(error1, channel) { 
+             if (error1) {
+                throw error1;
+            }        
+            var queue = logChannel;
+
+            channel.assertQueue(queue, {
+                durable: false
+             });
+
+            channel.sendToQueue(queue, Buffer.from(data));
+            console.log("logChannel Kuyruğuna Giden Veri : %s", data);
+        });
+        setTimeout(function() { 
+            connection.close(); 
+            process.exit(0) 
+        },500);
+    });
+}
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+ // MONGODB
+mongoose.connect('mongodb://localhost/logop', {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.set('useFindAndModify',false);
+
+
+var logSchema = new mongoose.Schema({
+
+   CreatedTime : {type : Date, default:Date.now},
+    logObject  :  Object
+});
+
+var logging =mongoose.model('logging', logSchema, 'objlog');
+
+app.post('/insert',async(req,res)=>{        
+    try {
+        var obj = new logging(req.body);  
+        sendRabbitMQ("logChannel", JSON.stringify(obj));             
+        res.send();
+
+    } 
+    catch (error) {
+
+        return res.status(500).send(error);    
+     }
+ });
+
+app.listen(3000,()=>{
+    console.log('Server is listening on port 3000');
+})
+
